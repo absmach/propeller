84436a7cd0764f6b3f97fbe6f5644599dc34ca67
JeffMboya <jangina.mboya@gmail.com>
2026-01-18 13:21:51 +0300

    Fix unused imports/functions
    Signed-off-by: JeffMboya <jangina.mboya@gmail.com>

--- manager/service_test.go
+++ manager/service_test.go
@@ -2,9 +2,6 @@ package manager
 
 import (
 	"testing"
-
-	"github.com/absmach/propeller/pkg/scheduler"
-	"github.com/absmach/propeller/pkg/storage"
 )
 
 // NOTE: The aggregation methods (aggregateJSONF64, aggregateConcat, aggregateRound)
@@ -23,31 +20,4 @@ func TestAggregateConcat(t *testing.T) {
 
 func TestAggregateRound(t *testing.T) {
 	t.Skip("Aggregation methods removed - aggregation is now handled by FL Coordinator")
-}
-
-func contains(s, substr string) bool {
-	return len(s) >= len(substr) && (s == substr || len(substr) == 0 ||
-		(len(s) > len(substr) && (s[:len(substr)] == substr ||
-			s[len(s)-len(substr):] == substr ||
-			containsMiddle(s, substr))))
-}
-
-func containsMiddle(s, substr string) bool {
-	for i := 0; i <= len(s)-len(substr); i++ {
-		if s[i:i+len(substr)] == substr {
-			return true
-		}
-	}
-	return false
-}
-
-// Test helper to create a mock service
-func createTestService() *service {
-	return &service{
-		tasksDB:       storage.NewInMemoryStorage(),
-		propletsDB:    storage.NewInMemoryStorage(),
-		taskPropletDB: storage.NewInMemoryStorage(),
-		metricsDB:     storage.NewInMemoryStorage(),
-		scheduler:     scheduler.NewRoundRobin(),
-	}
 }
