927b5099ecff386b5f8f5a5d9e2ebac2503ba2d3
JeffMboya <jangina.mboya@gmail.com>
2026-03-23 13:48:21 +0300

    feat(task): add wasm_http_url field to task and manager dispatch

--- manager/service.go
+++ manager/service.go
@@ -1679,6 +1679,7 @@ func (svc *service) publishStart(ctx context.Context, t task.Task, propletID str
 		"name":               t.Name,
 		"state":              t.State,
 		"image_url":          t.ImageURL,
+		"wasm_http_url":      t.WasmHTTPURL,
 		"file":               t.File,
 		"inputs":             t.Inputs,
 		"cli_args":           t.CLIArgs,
